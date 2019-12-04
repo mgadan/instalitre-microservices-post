5b82525d3d9ac871f113960387e590a18b5dc945
Simon PEROLS <simon.perols@gmail.com>
2019-12-04 10:44:31 +0000

    WIP: test manually fix routes

--- src/post/router.rs
+++ src/post/router.rs
@@ -3,25 +3,25 @@ use crate::post::handler;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::resource("/")
+        web::resource("/post")
             .route(web::post().to(handler::upload))
     );
 
     cfg.service(
-        web::resource("/{id}")
+        web::resource("/post/{id}")
             .route(web::get().to_async(handler::get))
             .route(web::delete().to_async(handler::delete))
             .route(web::put().to_async(handler::put))
     );
     
     cfg.service(
-        web::resource("/user/{id}")
+        web::resource("/post/user/{id}")
             .route(web::get().to(handler::get_all))
             .route(web::delete().to(handler::delete_all))
     );
 
     cfg.service(
-        web::resource("/file/{author}/{post}")
+        web::resource("/post/file/{author}/{post}")
             .route(web::get().to(handler::get_file))
     );
 }
